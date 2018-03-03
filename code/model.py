6948bb3a0f11d892e7318f3b953578af92626d0c
Dmitry Petrov <dmitry.petrov@nevesomo.com>
2018-03-03 01:21:33 +0000

    Pretrained VGG16

--- code/model.py
+++ code/model.py
@@ -1,55 +1,65 @@
 import os
+import numpy as np
 from keras import layers
 from keras import models
 from keras import optimizers
+from keras.applications import VGG16
 from keras.preprocessing.image import ImageDataGenerator
 import pickle
 
 import conf
 
+conv_base = VGG16(weights='imagenet',
+                  include_top=False,
+                  input_shape=(150, 150, 3))
+
 model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu',
-                        input_shape=(150, 150, 3)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(128, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(128, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Flatten())
-model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(256, activation='relu', input_dim=4 * 4 * 512))
+model.add(layers.Dropout(0.5))
 model.add(layers.Dense(1, activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy',
-              optimizer=optimizers.RMSprop(lr=1e-4),
+              optimizer=optimizers.RMSprop(lr=2e-5),
               metrics=['acc'])
 
 # All images will be rescaled by 1./255
-train_datagen = ImageDataGenerator(rescale=1./255)
-test_datagen = ImageDataGenerator(rescale=1./255)
+datagen = ImageDataGenerator(rescale=1./255)
 
-train_generator = train_datagen.flow_from_directory(
-        # This is the target directory
-        conf.train_dir,
-        # All images will be resized to 150x150
+batch_size = 20
+
+def extract_features(directory, sample_count):
+    features = np.zeros(shape=(sample_count, 4, 4, 512))
+    labels = np.zeros(shape=(sample_count))
+    generator = datagen.flow_from_directory(
+        directory,
         target_size=(150, 150),
-        batch_size=20,
-        # Since we use binary_crossentropy loss, we need binary labels
+        batch_size=batch_size,
         class_mode='binary')
+    i = 0
+    for inputs_batch, labels_batch in generator:
+        features_batch = conv_base.predict(inputs_batch)
+        features[i * batch_size : (i + 1) * batch_size] = features_batch
+        labels[i * batch_size : (i + 1) * batch_size] = labels_batch
+        i += 1
+        if i * batch_size >= sample_count:
+            # Note that since generators yield data indefinitely in a loop,
+            # we must `break` after every image has been seen once.
+            break
+    return features, labels
 
-validation_generator = test_datagen.flow_from_directory(
-        conf.validation_dir,
-        target_size=(150, 150),
-        batch_size=20,
-        class_mode='binary')
+train_features, train_labels = extract_features(conf.train_dir, 2000)
+validation_features, validation_labels = extract_features(conf.validation_dir, 1000)
+test_features, test_labels = extract_features(conf.test_dir, 1000)
 
-hist = model.fit_generator(
-      train_generator,
-      steps_per_epoch=100,
+train_features = np.reshape(train_features, (2000, 4 * 4 * 512))
+validation_features = np.reshape(validation_features, (1000, 4 * 4 * 512))
+test_features = np.reshape(test_features, (1000, 4 * 4 * 512))
+
+hist = model.fit(
+      train_features, train_labels,
       epochs=30,
-      validation_data=validation_generator,
-      validation_steps=50)
+      batch_size=20,
+      validation_data=(validation_features, validation_labels))
 
 model.save(os.path.join(conf.data_dir, 'model.h5'))
 pickle.dump(hist.history, open(os.path.join(conf.data_dir, 'history.p'), 'wb'))
